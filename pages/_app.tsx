7009c1850dedad0c46c9e021b0c0ccdf1220c1da
roger <roger@policiacivil.pe.gov.br>
2024-12-10 13:03:41 -0300

    ajustes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const API_URL = process.env.STRAPI_URL || "http://127.0.0.1:1337";
+
+export const client = new ApolloClient({
+  uri: `${API_URL}/graphql`,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    mutate: {
+      errorPolicy: "all",
+    },
+    query: {
+      errorPolicy: "all",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
